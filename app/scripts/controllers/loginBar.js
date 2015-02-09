c0f7998e13f4f33588ed0fffa81c558dc7f090dc
Brendan Batliner <brendan.batliner@gmail.com>
2015-02-09 09:54:40 -0600

    Login, current user, and logout button in nav bar.

--- app/scripts/controllers/loginBar.js
+++ app/scripts/controllers/loginBar.js
@@ -1,5 +1,12 @@
 angular.module('myTodoAngularApp')
 	.controller('LoginBarController', ['$scope', '$rootScope', function ($scope, $rootScope) {
-		$scope.user = $rootScope.user;
+		
 		$scope.hello = 'Hello!';
+		$scope.user = 'empty!';
+
+		$scope.$watch(function() {
+			return $rootScope.user;
+		}, function() {
+			$scope.user = $rootScope.user;
+		});
 	}]);
